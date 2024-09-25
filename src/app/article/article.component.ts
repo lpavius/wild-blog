4a3ec17b1a4135524f445370cc81cc6766138b35
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-09-26 00:54:48 +0200

    challenge completed

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
+  article = {
+    title: 'Bienvenue !',
+    author: 'Lucy',
+    content: 'Je ne sais pas encore de quoi va parler ce Blog.',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: ''
+  }
 
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
