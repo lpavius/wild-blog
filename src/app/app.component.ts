7468f3a9096987bead27f49d2023ea78146f522b
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-09-26 15:33:19 +0200

    ajout tab articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from "./article/article.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleComponent],
+  imports: [RouterOutlet, ArticleComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Bienvenue sur le Wild Blog de Lucy !';
+
+  articles = [
+    {
+      title: 'Angular 16: Les nouveautés',
+      author: 'Alice',
+      content: 'Les nouveautés d\'Angular 16 incluent...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 120
+    },
+    {
+      title: 'Développer une API REST',
+      author: 'Bob',
+      content: 'Développer une API REST nécessite...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: false,
+      comment: '',
+      likes: 75
+    },
+    {
+      title: 'Pourquoi TypeScript est essentiel ?',
+      author: 'Charlie',
+      content: 'TypeScript apporte de la robustesse...',
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true,
+      comment: '',
+      likes: 200
+    }
+  ];
+
 }
