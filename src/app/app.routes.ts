216e0ac9ecaf695d81037368d1c5303dd2487922
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-10-01 14:22:56 +0200

    ajout new component contactForm et route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'article/:id', component: ArticlePageComponent },
+  { path: 'contact', component: ContactFormComponent},
   { path: '**', component: NotFoundComponent }
 ];
