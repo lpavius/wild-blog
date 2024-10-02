8ec3b66d7b80f92dba0bb58ba2ce0ca83b5aee54
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-10-02 11:16:18 +0200

    creation formulaire avec champ username, email et bouton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'article/:id', component: ArticlePageComponent },
   { path: 'contact', component: ContactFormComponent},
+  { path: 'signup', component: SignupFormComponent},
   { path: '**', component: NotFoundComponent }
 ];
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.scss'
 })
 export class SignupFormComponent {
+
+  formBuilder: FormBuilder = inject(FormBuilder)
+
+  signUpForm: FormGroup = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]]
+  })
+
+  // signUpForm: FormGroup
+
+  // constructor(private formBuilder: FormBuilder) {
+  //   this.signUpForm = this.formBuilder.group({
+  //     username: ['', Validators.required, Validators.minLength(3)],
+  //     email: ['', [Validators.required, Validators.email]],
+  //   });
+  // }
+
+  onSubmit(): void {
+
+    if (this.signUpForm.valid) {
+      console.log('Formulaire envoyé avec succès', this.signUpForm.value)
+    } else {
+      console.log('Formulaire invalide')
+    }
+  }
 
 }
