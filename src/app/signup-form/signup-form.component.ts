2149c025cf1ab15729c518a1db8b14414b98d72a
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-10-02 14:18:44 +0200

    ajout champs MP et confirmer le MP

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-form',
@@ -12,19 +12,40 @@ export class SignupFormComponent {
 
   formBuilder: FormBuilder = inject(FormBuilder)
 
-  signUpForm: FormGroup = this.formBuilder.group({
+  signUpForm = this.formBuilder.group({
     username: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required, Validators.email]]
+    email: ['', [Validators.required, Validators.email]],
+    passwords: this.formBuilder.group({
+      password: ['', [Validators.required, this.securePasswordValidator()]],
+      confirmPassword: [''] }, {
+        validators: this.passwordMatchValidator()
+      })
   })
 
-  // signUpForm: FormGroup
+  passwordMatchValidator(): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const password = formGroup.get('password')?.value
+      const confirmPassword = formGroup.get('confirmPassword')?.value
 
-  // constructor(private formBuilder: FormBuilder) {
-  //   this.signUpForm = this.formBuilder.group({
-  //     username: ['', Validators.required, Validators.minLength(3)],
-  //     email: ['', [Validators.required, Validators.email]],
-  //   });
-  // }
+      return password === confirmPassword ? null : { passwordsMismatch: true }
+    }
+  }
+
+  securePasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value || '';
+
+      const hasUpperCase = /[A-Z]/.test(value)
+      const hasLowerCase = /[a-z]/.test(value)
+      const hasNumber = /\d/.test(value)
+      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value)
+      const isValidLength = value.length >= 12
+
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isValidLength
+
+      return passwordValid ? null : { securePassword: true }
+    }
+  }
 
   onSubmit(): void {
 
